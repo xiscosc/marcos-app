89808b597be13c372331b647eddecc69d3de42bf
Xisco Sastre Cabot <xiscosastre@gmail.com>
2025-04-13 12:15:39 +0200

    store config in local storage

--- apps/marcos/src/lib/stores/profiler.store.ts
+++ apps/marcos/src/lib/stores/profiler.store.ts
@@ -1,9 +1,17 @@
 import { get, writable } from 'svelte/store';
 import { PUBLIC_PROFILER_CONFIG } from '$env/static/public';
 import { Profiler, type ProfilerConfig } from '@/shared/profiling/profiler';
+import { DateTime } from 'luxon';
+import { browser } from '$app/environment';
 
-const profilerConfig = writable(Profiler.parseConfig(PUBLIC_PROFILER_CONFIG));
+const profilerConfigKey = 'mmss-profiler-config';
+const expirationTimeInMillis = 1000 * 60 * 60 * 24;
 const globalProfiler = writable<Profiler | undefined>();
+
+type ProfilerLocalStorage = {
+	expiration: number;
+	config: string;
+};
 
 export function getGlobalProfiler(): Profiler {
 	const profiler = get(globalProfiler);
@@ -15,10 +23,28 @@ export function getGlobalProfiler(): Profiler {
 }
 
 export function getProfilerConfig(): ProfilerConfig {
-	return get(profilerConfig);
+	if (browser) {
+		const profilerStoredConfig = localStorage.getItem(profilerConfigKey);
+		if (profilerStoredConfig) {
+			const storedData: ProfilerLocalStorage = JSON.parse(profilerStoredConfig);
+			const isExpired = DateTime.now().toMillis() > storedData.expiration;
+			if (!isExpired) {
+				return Profiler.parseConfig(storedData.config);
+			}
+		}
+	}
+
+	return Profiler.parseConfig(PUBLIC_PROFILER_CONFIG);
 }
 
 export function updateProfilerConfig(newConfig: ProfilerConfig): void {
-	profilerConfig.set(newConfig);
-	globalProfiler.set(new Profiler(newConfig));
+	if (browser) {
+		const localStorageConfig: ProfilerLocalStorage = {
+			expiration: DateTime.now().plus({ milliseconds: expirationTimeInMillis }).toMillis(),
+			config: Profiler.encodeConfig(newConfig)
+		};
+
+		localStorage.setItem(profilerConfigKey, JSON.stringify(localStorageConfig));
+		globalProfiler.set(new Profiler(newConfig));
+	}
 }
